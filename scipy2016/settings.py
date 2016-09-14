abbb48fada2b23bd8fc7f317f15a86b6d8db9eeb
Thomas Stephen Lee <lee@fossee.in>
2016-09-14 11:59:54 +0530

    Second Commit

--- scipy2016/settings.py
+++ scipy2016/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': 'scipy2016',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'aero123',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',
     }
