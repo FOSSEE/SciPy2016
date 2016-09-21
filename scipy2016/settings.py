ac843b78881ade253a0c0b8d2f86534deee0de31
komalsheth286 <komalsheth286@gmail.com>
2016-09-21 11:38:11 +0530

    CSS files added

--- scipy2016/settings.py
+++ scipy2016/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': 'scipy2016',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'aero123',
+        'PASSWORD': 'root',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',
     }
